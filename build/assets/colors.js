988df47760382e84ebb18e6cf56aefc6a75bc2e0
Hubert@Home <hubba2089@gmail.com>
2022-05-14 01:19:16 +0200

    remove build

--- build/assets/colors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var colorValues;
-(function (colorValues) {
-    colorValues["embedDefault"] = "#531D89";
-})(colorValues || (colorValues = {}));
-exports.default = colorValues;
--- build/assets/emojis.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var emojis;
-(function (emojis) {
-    emojis["DZBANEK"] = "<:dzbanek:925423201218080839>";
-})(emojis || (emojis = {}));
-exports.default = emojis;
