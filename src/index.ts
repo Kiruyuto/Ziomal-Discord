cdc01ad12cd5bc93477a1550eedbe05084c54981
Hubert@Home <hubba2089@gmail.com>
2022-05-15 16:32:41 +0200

    Small index changes

--- src/index.ts
+++ src/index.ts
@@ -8,30 +8,29 @@ import 'dotenv/config'
 import colorValues from './assets/colors'
 
 const client = new DCJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ],
-    allowedMentions: {
-      roles: ['roles', 'users', 'everyone'],
-      repliedUser: false,
-    },
-    presence: {
-      status: "dnd",
-      activities: [{
-        name: 'Songs and stuff',
-        type: 'PLAYING',
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MEMBERS
+  ],
+  allowedMentions: {
+    parse: ['users', 'roles'],
+    repliedUser: false,
+  },
+  presence: {
+    status: "dnd",
+    activities: [{
+      name: 'Songs and stuff',
+      type: 'PLAYING',
     }]
-    },
-    restTimeOffset: 0,
-
+  },
+  restTimeOffset: 0,
 })
 
 // Unlimited event listeners
-client.setMaxListeners(0) 
+client.setMaxListeners(0)
 
 // Distube options - details in docs
 export const distube = new DisTube(client, {
@@ -55,46 +54,46 @@ export const distube = new DisTube(client, {
   nsfw: true,
   emitAddSongWhenCreatingQueue: true,
   emitAddListWhenCreatingQueue: true,
-  plugins: [ 
+  plugins: [
     new YtDlpPlugin()
   ],
 })
 
 client.on('ready', () => {
-    const guild = client.guilds.cache.get(IDs.GUILD)
-    if(!guild) { return console.error('Guild not found') }
+  const guild = client.guilds.cache.get(IDs.GUILD)
+  if (!guild) { return console.error('Guild not found') }
 
-    const wok = new WOK(client, {
-      commandDir: path.join(__dirname, 'commands'), 
-      featureDir: path.join(__dirname, 'features'),
-      messagesPath: path.join(__dirname, 'languages/langs.json'),
-      defaultLanguage: 'english',
-      testServers: IDs.GUILD_TEST, 
-      botOwners: IDs.KIRU, 
-      delErrMsgCooldown: -1, 
-      ignoreBots: true,
-      typeScript: false, 
-      mongoUri: process.env.MONGO_URI,
-      //debug: true, // Display more information in the console
-      //showWarns: true, // Display warnings in the console
-    })
-    .setDefaultPrefix('!') 
+  const wok = new WOK(client, {
+    commandDir: path.join(__dirname, 'commands'),
+    featureDir: path.join(__dirname, 'features'),
+    messagesPath: path.join(__dirname, 'languages/langs.json'),
+    defaultLanguage: 'english',
+    testServers: IDs.GUILD_TEST,
+    botOwners: IDs.KIRU,
+    delErrMsgCooldown: -1,
+    ignoreBots: true,
+    typeScript: process.env.TYPESCRIPT === 'true' ? true : false,
+    mongoUri: process.env.MONGO_URI,
+    //debug: true, // Display more information in the console
+    //showWarns: true, // Display warnings in the console
+  })
+    .setDefaultPrefix('!')
 
-    // Wok events
-    wok.on('databaseConnected', async (connection: any, state: any) => {
-      console.log(`Database connection state is [${state}]\n`)
-    })
+  // Wok events
+  wok.on('databaseConnected', async (connection: any, state: any) => {
+    console.log(`Database connection state is [${state}]\n`)
+  })
 
-    wok.on('languageNotSupported', (guild: any, lang: any) => {
-      console.log(`"${guild.name}" Attempted to set language to "${lang}"`)
-    })
+  wok.on('languageNotSupported', (guild: any, lang: any) => {
+    console.log(`"${guild.name}" Attempted to set language to "${lang}"`)
+  })
 
-    wok.on('commandException', (command: any, message: any, error: any) => {
-      console.log(`An exception occured when using command "${command.names[0]}"! The error is:`)
-      console.error(error)
-    })
- 
-  const guildCounter  = client.guilds.cache.size
+  wok.on('commandException', (command: any, message: any, error: any) => {
+    console.log(`An exception occured when using command "${command.names[0]}"! The error is:`)
+    console.error(error)
+  })
+
+  const guildCounter = client.guilds.cache.size
   console.log(`\n[${client.user?.tag}] is online!\nCurrently used in: [${guildCounter} guilds]`)
 
   // distube events
@@ -111,7 +110,7 @@ client.on('ready', () => {
       color: colorValues.embedDefault,
     })
     client.users.fetch(IDs.KIRU)
-    .then(user => { user.send({ content: `${error}`, embeds: [x] }) })
+      .then(user => { user.send({ content: `${error}`, embeds: [x] }) })
     let ChannelEmbed = new DCJS.MessageEmbed({ description: ':x: An error with music player occured! Message has been sent to developer.' })
     channel.send({ embeds: [ChannelEmbed] })
   })
